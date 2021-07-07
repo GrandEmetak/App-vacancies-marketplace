b11f913799d76ccde678f7a238260e13d1cf3db2
SlartiBartFast-art <JekHamond@tutanota.com>
2021-07-07 15:59:34 +0500

    2.3. Загрузка деталей поста. [#285212] #2.0

--- src/main/java/ru/job4j/grabber/utils/Post.java
+++ src/main/java/ru/job4j/grabber/utils/Post.java
@@ -1,14 +1,7 @@
 package ru.job4j.grabber.utils;
 
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
-
 import java.io.IOException;
 import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * 2.3. Загрузка деталей поста. [#285212]
@@ -22,17 +15,6 @@ public class Post {
     private String link;
     private String description;
     private LocalDateTime created;
-
-    /**
-     * Кнструктор, при создании экземпляра объекта передается ссылка в параметры,
-     * для дальнейшего парсинга данной страницы и записи через метод loader,
-     * данных в поля объекта
-     * @param link ссылка на страницу для парсинга и заполнения полей экземпляра класса
-     * @throws IOException
-     */
-    public Post(String link) throws IOException {
-        loader(link);
-    }
 
     public int getId() {
         return id;
@@ -54,61 +36,24 @@ public class Post {
         return created;
     }
 
-    /**
-     * Метод проводит запись в поля объекат(id, title, link, description, created(LocalDateTime)) Post,
-     * при его создании.
-     * @param link ссылка на страницу вакансии
-     * @throws IOException
-     */
-    private void loader(String link) throws IOException {
-        //Экземпляр класса с методом для конвертации даты в формат LocalDateTime
-        SqlDateTimeParser sqlDateTimeParser = new SqlDateTimeParser();
-
-        this.link = link; // zapisali link na stranicy vacancii
-        Document document = Jsoup.connect(link).get();
-        Elements elements = document.select(".msgTable");
-        var tr = elements.select("td[class=messageHeader]");
-        var elo = tr.get(0);
-        String ty = elo.attr("id");
-        String[] arStr = ty.split("id");
-        // zapisali id
-        this.id = Integer.parseInt(arStr[1]);
-        //название вакансии
-        String vacStr = elo.text();
-        this.title = vacStr; // zapisali nazvanie vakansii
-//описание вакансии body
-        Elements elements1 = document.select(".msgBody");
-        Map<Integer, String> mapStr = new HashMap<>();
-        int i = 0;
-        for (Element element : elements1) {
-            mapStr.put(i++, element.text());
-        }
-        this.description = mapStr.get(1); // zapisali opisanie vacancii
-// дата
-        Elements dat = elements.select("td[class=msgFooter]");
-        boolean msdt = dat.isEmpty();
-        String[] daTeAr = dat.text().split(" ");
-        String bild = daTeAr[0] + " " + daTeAr[1] + " " + daTeAr[2] + " " + daTeAr[3];
-        var time = sqlDateTimeParser.parse(bild);
-        this.created = time;
+    public void setId(int id) {
+        this.id = id;
     }
 
-    public static void main(String[] args) throws IOException {
-//несколько разных ссылок на разные вакансии
-// String link = "https://www.sql.ru/forum/1325330/lidy-be-fe-senior-cistemnye-analitiki-qa-i-devops-moskva-do-200t";
-// String link = "https://www.sql.ru/forum/1336128/arhitektor-resheniy-moskva-300-000-350-000-rub";
-        String link = "https://www.sql.ru/forum/1337113/frontend-razrabotchik-remote-do-330-000";
-        Post post = new Post(link);
-        System.out.println();
-        System.out.println(post.getId());
-        System.out.println();
-        System.out.println(post.getTitle());
-        System.out.println();
-        System.out.println(post.getLink());
-        System.out.println();
-        System.out.println(post.getDescription());
-        System.out.println();
-        System.out.println(post.getCreated());
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setCreated(LocalDateTime created) {
+        this.created = created;
     }
 
 }
