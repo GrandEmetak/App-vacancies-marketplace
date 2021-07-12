9681d83e09b36e9378744616a974b01e00acd420
SlartiBartFast-art <73285357+SlartiBartFast-art@users.noreply.github.com>
2021-07-12 20:34:23 +0500

    Add files via upload
    Rewrite Query

--- src/main/java/ru/job4j/sql/companyQuery.sql
+++ src/main/java/ru/job4j/sql/companyQuery.sql
@@ -1,15 +1,12 @@
 /* 1. В одном запросе получить:
 - имена всех person, которые не состоят в компании с id = 5;
 - название компании для каждого человека.*/
-select s.name, ss.name from person as s join company as ss on s.company_id = ss.id
-where not company_id in (5);
+/*select s.name, ss.name from person as s join company as ss on s.company_id = ss.id
+where not company_id in (5);*/
 /* 2.Необходимо выбрать название компании с максимальным количеством человек + количество человек в этой компании.*/
-/*SELECT c.name, COUNT(*) cnt
+
+SELECT c.name, COUNT(*) people
 FROM company c, person p
 WHERE c.id = p.company_id
 GROUP BY c.name
-HAVING COUNT(*) = (SELECT COUNT(pp.*) 
-                   FROM person pp
-                   GROUP BY pp.company_id
-                   ORDER BY 1 DESC LIMIT 1
-				   );*/
+ORDER BY 2 DESC LIMIT 1
